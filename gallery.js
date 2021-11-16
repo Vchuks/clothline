8afa3a888c6d082bba103b235e72f709e0631df6
Vchuks <64593598+Vchuks@users.noreply.github.com>
2021-11-17 00:22:23 +0100

    For array

---
+++ gallery.js
@@ -0,0 +1,85 @@
+const gallery =[
+    {
+        card_img:"slide-1",
+        name:"  White Top",
+        price:"$60"
+    },
+    {
+        card_img:"product-1",
+        name:"Sneakers",
+        price:"$40"
+    },
+    {
+        card_img:"slide-1",
+        name:"Jackets",
+        price:"$80"
+    },
+    {
+        card_img:"you-may2",
+        name:"Jeans and T-Shirt",
+        price:"$100"
+    },
+    {
+        card_img:"you-may3",
+        name:"Coat",
+        price:"$40"
+    },
+    {
+        card_img:"you-may4",
+        name:"Jeans and Boot",
+        price:"$120"
+    },
+    {
+        card_img:"slide-1",
+        name:"Sneakers",
+        price:"$40"
+    },
+    {
+        card_img:"product-1",
+        name:"Sneakers",
+        price:"$40"
+    },
+    {
+        card_img:"product-1",
+        name:"Sneakers",
+        price:"$40"
+    },
+    {
+        card_img:"you-may2",
+        name:"Jeans and T-Shirt",
+        price:"$100"
+    },
+    {
+        card_img:"you-may3",
+        name:"Coat",
+        price:"$40"
+    },
+    {
+        card_img:"you-may4",
+        name:"Jeans and Boot",
+        price:"$120"
+    },
+
+]
+
+let galleryinfo = document.getElementById("gallery");
+let html ="";
+
+gallery.forEach(e => {
+    html += `
+    <div class="card my-5" style="width: 20rem;>
+    <div class="card-image">
+    <img src="../images/${e.card_img}.jpg" class="card-img-top" alt="cloth">
+    <div class="card-body">
+      <p class="card-text"><h4>${e.name}</h4></p>
+      <h3>${e.price}</h3>
+    </div>
+    </div>
+  </div>
+    `
+    console.log(e)
+    galleryinfo.innerHTML = html;
+
+});
+
+
---
+++ style.js
@@ -0,0 +1,39 @@
+const homegallery =[
+    {
+        card_img:"slide-1",
+        name:"  White Top",
+        price:"$60"
+    },
+    {
+        card_img:"product-1",
+        name:"Sneakers",
+        price:"$40"
+    },
+    {
+        card_img:"slide-1",
+        name:"White Top",
+        price:"$80"
+    }
+]
+
+let homegalleryinfo = document.getElementById("home-gallery");
+let html ="";
+
+homegallery.forEach(e => {
+    html += `
+    <div class="card my-5" style="width: 20rem;>
+    <div class="card-image">
+    <img src="../images/${e.card_img}.jpg" class="card-img-top" alt="cloth">
+    <div class="card-body">
+      <p class="card-text"><h4>${e.name}</h4></p>
+      <h3>${e.price}</h3>
+    </div>
+    </div>
+  </div>
+    `
+    console.log(e)
+    homegalleryinfo.innerHTML = html;
+
+});
+
+
